d00561169fb964408eed8e32a031d99210dc161b
Samuel Okuboyejo <78604165+samuelokuboyejo@users.noreply.github.com>
2025-04-16 00:19:55 +0100

    Update AuthController.java

--- SimpleOrder/src/main/java/com/project/simple_order/auth/controller/AuthController.java
+++ SimpleOrder/src/main/java/com/project/simple_order/auth/controller/AuthController.java
@@ -7,11 +7,13 @@ import com.project.simple_order.auth.utils.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
